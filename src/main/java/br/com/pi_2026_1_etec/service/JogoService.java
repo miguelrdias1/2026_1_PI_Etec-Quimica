5b35cf936a08cec453b8d21b50570f816a8ff7b7
Isabela Nakai <bebelanakai@gmail.com>
2026-05-14 20:02:40 -0300

    fix: JogoService.java

--- src/main/java/br/com/pi_2026_1_etec/service/JogoService.java
+++ src/main/java/br/com/pi_2026_1_etec/service/JogoService.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class JogoService {
     Scanner scanner = new Scanner(System.in);
     public void iniciar () {
-        Questao p1 = new Questao();
+        br.com.pi_2026_1_etec.service.JogoService p1 = new br.com.pi_2026_1_etec.service.JogoService();
         p1.enunciado = "Qual o nome desse material";
     }
 }
