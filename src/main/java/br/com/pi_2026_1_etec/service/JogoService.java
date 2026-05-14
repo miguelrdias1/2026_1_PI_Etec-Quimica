16072aa66231d3650505a0dd6335f2de02547ff7
Isabela Nakai <bebelanakai@gmail.com>
2026-05-14 20:20:31 -0300

    fix

--- src/main/java/br/com/pi_2026_1_etec/service/JogoService.java
+++ src/main/java/br/com/pi_2026_1_etec/service/JogoService.java
@@ -6,6 +6,5 @@ public class JogoService {
     Scanner scanner = new Scanner(System.in);
     public void iniciar () {
         br.com.pi_2026_1_etec.service.JogoService p1 = new br.com.pi_2026_1_etec.service.JogoService();
-        p1.enunciado = "Qual o nome desse material";
     }
 }
