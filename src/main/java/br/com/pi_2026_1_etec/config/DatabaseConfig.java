77188b2d3879eb7241515e23d976d9b429faa130
Gabriel Araujo <25.11742-5@maua.br>
2026-05-20 20:36:04 -0300

    FEAT: DB Connection

---
+++ src/main/java/br/com/pi_2026_1_etec/config/DatabaseConfig.java
@@ -0,0 +1,24 @@
+
+package br.com.pi_2026_1_etec.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class ConexaoBD {
+    private static Dotenv dotenv = Dotenv.load();
+    private static String host   = dotenv.get("DB_HOST");
+    private static String porta  = dotenv.get("DB_PORT");
+    private static String db     = dotenv.get("DB_NAME");
+    private static String usuario = dotenv.get("DB_USER");
+    private static String senha  = dotenv.get("DB_PASSWORD");
+
+
+public static Connection obterConexao() throws Exception{
+    String url = String.format(
+        "jdbc:mysql://%s:%s/%s",
+    host, porta, db
+    );
+    return DriverManager.getConnection(url, usuario, senha);
+    }
+}
