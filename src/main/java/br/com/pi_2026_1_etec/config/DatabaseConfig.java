4427f60957e8f83a40337b2aa1859118724a52fd
Gabriel Araujo <25.11742-5@maua.br>
2026-05-20 20:36:47 -0300

    FEAT: DB Connection

--- src/main/java/br/com/pi_2026_1_etec/config/DatabaseConfig.java
+++ src/main/java/br/com/pi_2026_1_etec/config/DatabaseConfig.java
@@ -5,13 +5,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import io.github.cdimascio.dotenv.Dotenv;
 
+
 public class ConexaoBD {
     private static Dotenv dotenv = Dotenv.load();
-    private static String host   = dotenv.get("DB_HOST");
-    private static String porta  = dotenv.get("DB_PORT");
-    private static String db     = dotenv.get("DB_NAME");
+    private static String host = dotenv.get("DB_HOST");
+    private static String porta = dotenv.get("DB_PORT");
+    private static String db = dotenv.get("DB_NAME");
     private static String usuario = dotenv.get("DB_USER");
-    private static String senha  = dotenv.get("DB_PASSWORD");
+    private static String senha = dotenv.get("DB_PASSWORD");
 
 
 public static Connection obterConexao() throws Exception{
